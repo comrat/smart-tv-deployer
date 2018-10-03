d36f67da467ebfb0635d430fde8741e78d50d8b6
Alexey Pobery <poberyaa@gmail.com>
2018-10-03 08:54:41 +0300

    Add '--base-url' parameter to allow deploying apps with remotely located qml.app.js file

--- build.py
+++ build.py
@@ -145,6 +145,7 @@ parser.add_argument('--tv', '-t', help='TV name', dest='tv')
 parser.add_argument('--release', '-r', help='build release apk for android platform', dest='release', default=False)
 parser.add_argument('--debug', '-d', help='start debugging afer building', dest='debug', default=False)
 parser.add_argument('--app', '-a', help='target application if there is more than one apps in project', dest='app')
+parser.add_argument('--base-url', '-b', help='base URL value if you need to get qml.app.js file remotely', dest='baseurl')
 args = parser.parse_args()
 
 manifest_path = '.manifest'
@@ -156,6 +157,7 @@ debug = args.debug
 jobs = args.jobs
 minify = args.minify
 app = args.app
+baseurl = args.baseurl
 
 if platform is None:
 	print "Provide platform name: ./smart-tv-deployer.py -p <PLATFORM_NAME>"
@@ -168,7 +170,7 @@ if path.exists(manifest_path):
 	app_dir = "/" + args.app if args.app is not None else ""
 	print "Manifest parsed, title:", title, "version", version
 	print "Build project..."
-	os.system('./qmlcore/build %s -p %s -j %s' %("-m" if minify else "", platform, jobs))
+	os.system('./qmlcore/build %s -p %s -j %s %s' %("-m" if minify else "", platform, jobs, "-s baseurl " + baseurl if baseurl is not None else ""))
 
 	if platform == "webos":
 		print "============== WEBOS DEPLOYMENT =============="
