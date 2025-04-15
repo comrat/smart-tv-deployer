d9c69af105011254bda4450ae1de7da8545d1a15
bogeyboy <deniskomsikov@gmail.com>
2025-04-15 23:16:06 +0300

    Update build.py

--- build.py
+++ build.py
@@ -171,7 +171,7 @@ def deploy_electron(app, electronjs_os):
 		os.system('npm start')
 
 
-def deploy_android(platform, title, release, app, android_build):
+def deploy_android(platform, title, release, buildOnly, app, android_build):
 	platform_folder = './build.' + platform + app
 	os.system('cd %s' %(platform_folder))
 	os.chdir(platform_folder)
@@ -182,16 +182,14 @@ def deploy_android(platform, title, release, app, android_build):
 
 	print('Run build.py...')
 	app_folder = title if not app else app[1:]
-	# TODO: WTF?! Cyclic call
-	# if release:
-	# 	os.system('../build.py -p androidtv --app %s -sp title %s --release' %(app_folder, app_folder))
-	# else:
-	# 	os.system('../build.py -p androidtv --app %s -sp title %s' %(app_folder, app_folder))
-	os.system('../qmlcore/platform/android/build.py --app %s --title %s %s' %(app_folder, app_folder, '--release' if release else ''))
+	os.system('./build.py --app %s --title %s %s' %(app_folder, app_folder, '--release' if release else ''))
 	print('Install via adb...')
 	apkFilePath = '%s/platforms/android/app/build/outputs/apk/debug/app-debug.apk' %title
 	if path.exists(apkFilePath):
-		os.system('adb install -r %s' %apkFilePath)
+		if buildOnly:
+			os.system('cp %s ./%s.apk' %(apkFilePath, app_folder))
+		else:
+			os.system('adb install -r %s' %apkFilePath)
 	else:
 		print('No .apk file at path %s' %apkFilePath)
 
@@ -221,6 +219,7 @@ parser.add_argument('--os', '-os', help='target electronjs OS', dest='electronjs
 parser.add_argument('--tizen-profile', '-tp', help='tizen studio profile path', dest='tizen_profile')
 parser.add_argument('--tv', '-t', help='TV name', dest='tv')
 parser.add_argument('--release', '-r', help='generate release code (no logs)', default = False, action = 'store_true')
+parser.add_argument('--buildOnly', '-B', help='generate apk file (without deploy)', default = False)
 parser.add_argument('--debug', '-d', help='start debugging after building', dest='debug', default=False)
 parser.add_argument('--app', '-a', help='target application if there is more than one apps in project', dest='app')
 parser.add_argument('--base-url', '-b', help='base URL value if you need to get qml.app.js file remotely', dest='baseurl')
@@ -235,6 +234,7 @@ electronjs_os = args.electronjs_os
 platform = args.platform
 tv = args.tv
 release = args.release
+buildOnly = args.buildOnly
 debug = args.debug
 jobs = args.jobs
 minify = args.minify
@@ -280,9 +280,9 @@ if path.exists(manifest_path):
 	elif platform == 'orsay':
 		deploy_orsay(title, version, app_dir)
 	elif platform == 'androidtv':
-		deploy_android('androidtv', title, release, app_dir, android_build)
+		deploy_android('androidtv', title, release, buildOnly, app_dir, android_build)
 	elif platform == 'android':
-		deploy_android('android', title, release, app_dir, android_build)
+		deploy_android('android', title, release, buildOnly, app_dir, android_build)
 	elif platform == 'androidnative':
 		deploy_android_native(title, release, app_dir)
 	elif platform == 'ios':
